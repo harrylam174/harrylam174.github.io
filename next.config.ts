7ec815d26496a5d691cbdff2c951be36bec897eb
Harry Lam <122658081+harrylam174@users.noreply.github.com>
2025-08-01 16:03:30 +0700

    Fix something

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig = {
   output: 'export',
-  basePath: isGithubPages ? '/your-repo-name' : '',
-  assetPrefix: isGithubPages ? '/your-repo-name/' : '',
+  basePath: isGithubPages ? '/harrylam174.github.io' : '',
+  assetPrefix: isGithubPages ? '/harrylam174.github.io/' : '',
 };
 
 module.exports = nextConfig;
